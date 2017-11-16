e253f10ba2f0883da0be602f37a75d1d881929c1
NoahOrberg <noah.orberg.9999@gmail.com>
2017-11-16 11:11:05 +0900

    fix config

--- config/config.go
+++ config/config.go
@@ -1,9 +1,23 @@
 package config
 
+import (
+	"github.com/kelseyhightower/envconfig"
+)
+
 type AOJConfig struct {
-	Endpoint string `default:"https://judgeapi.u-aizu.ac.jp"`
+	Endpoint    string `default:"https://judgeapi.u-aizu.ac.jp"`
+	ID          string `default:""`
+	RawPassword string `default:""`
 }
 
-func GetConfig() (AOJConfig, error) {
-	return conf, nil
+var conf AOJConfig
+
+func init() {
+	_ = envconfig.Process("aoj", &conf)
+	// NOTE: not yet set Variable
+	conf.Endpoint = "https://judgeapi.u-aizu.ac.jp"
+}
+
+func GetConfig() AOJConfig {
+	return conf
 }
