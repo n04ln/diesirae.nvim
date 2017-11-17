a92c6e8d4a884727ed1a813795782e6c27796729
NoahOrberg <noah.orberg.9999@gmail.com>
2017-11-17 13:44:44 +0900

    add CurrentBufferFiletype

--- nvimutil/nvimutil.go
+++ nvimutil/nvimutil.go
@@ -1,6 +1,11 @@
 package nvimutil
 
-import "github.com/neovim/go-client/nvim"
+import (
+	"fmt"
+	"strings"
+
+	"github.com/neovim/go-client/nvim"
+)
 
 type Nvimutil struct {
 	v *nvim.Nvim
@@ -14,4 +19,30 @@ func New(v *nvim.Nvim) *Nvimutil {
 
 func (n *Nvimutil) Log(message string) error {
 	return n.v.Command("echom 'aoj.nvim: " + message + "'")
+}
+
+func (n *Nvimutil) CurrentBufferFileType() (string, error) {
+	buf, err := n.v.CurrentBuffer()
+	if err != nil {
+		return "", err
+	}
+
+	bufferName, err := n.v.BufferName(buf)
+	if err != nil {
+		return "", err
+	}
+
+	dotName := strings.Split(bufferName, ".")[len(strings.Split(bufferName, "."))-1]
+
+	var language string
+	switch dotName {
+	case "c":
+		language = "C"
+	case "hs": // NOTE: maybe not correct return "Haskell"? need fix
+		language = "Haskell"
+	default:
+		return "", fmt.Errorf("cannot submit this file: .%s", dotName)
+	}
+
+	return language, nil
 }
