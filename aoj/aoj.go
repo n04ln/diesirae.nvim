ca2493124db14c7b4fe0b8c5be8467ffb9292e71
NoahOrberg <noah.orberg.9999@gmail.com>
2017-11-17 10:19:52 +0900

    modify aoj package

--- aoj/aoj.go
+++ aoj/aoj.go
@@ -1,6 +1,10 @@
 package aoj
 
 import (
+	"encoding/json"
+	"fmt"
+	"time"
+
 	"github.com/NoahOrberg/aoj.nvim/config"
 	"github.com/NoahOrberg/aoj.nvim/util"
 	"github.com/h2non/gentleman"
@@ -37,6 +41,10 @@ func Session(id, rawPassword string) (string, error) {
 	return res.Cookies[0].Raw, nil
 }
 
+type SubmitRes struct {
+	Token string `json:"token"`
+}
+
 func Submit(cookie, problemId, language, sourceCode string) (string, error) {
 	conf := config.GetConfig()
 
@@ -62,5 +70,128 @@ func Submit(cookie, problemId, language, sourceCode string) (string, error) {
 		return "", util.ErrResponseIsNotOK
 	}
 
-	return res.String(), nil // TODO: return token only
+	var r SubmitRes
+	if err := json.Unmarshal([]byte(res.String()), &r); err != nil {
+		return "", err
+	}
+
+	return r.Token, nil
+}
+
+type errStatus struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+}
+
+type caseVerdicts struct {
+	// NOTE: I DO NOT know the following types
+	// CaseName   XXXXX `json:"caseName"`
+	// InputSize  XXXXX `json:"inputSize"`
+	// OutputSize XXXXX `json:"outputSize"`
+	CpuTime int64  `json:"cpuTime"`
+	Memory  int64  `json:"memory"`
+	Serial  int    `json:"serial"`
+	Label   string `json:"label"`
+	Status  string `json:"status"`
+}
+
+type SubmissionStatus struct {
+	CaseVerdicts []caseVerdicts `json:"caseVerdicts"`
+	CompileError string         `json:"compileError"`
+	RuntimeError string         `json:"runtimeError"`
+	UserOutput   string         `json:"userOutput"`
+}
+
+func Status(cookie, token string) (*SubmissionStatus, error) {
+	recents, err := getRecentSubmissionRecords(cookie)
+	if err != nil {
+		return nil, err
+	}
+
+	var submission recentSubmission
+	for _, r := range recents {
+		if r.Token == token {
+			submission = r
+		}
+	}
+
+	var cnt int
+RETRY:
+	res, err := getStatusByJudgeId(cookie, submission.JudgeId)
+	if err != nil {
+		return nil, err
+	}
+	if !res.Ok {
+		if cnt <= 120 {
+			time.Sleep(3 * time.Second)
+			cnt++
+			goto RETRY
+		}
+		return nil, util.ErrResponseIsNotOK
+	}
+
+	var substatus SubmissionStatus
+	if err := json.Unmarshal([]byte(res.String()), &substatus); err != nil {
+		return nil, err
+	}
+
+	return &substatus, nil
+}
+
+func getStatusByJudgeId(cookie string, judgeId int64) (*gentleman.Response, error) {
+	conf := config.GetConfig()
+	cli := gentleman.New()
+	cli.URL(conf.Endpoint)
+
+	req := cli.Request()
+	req.Method("GET")
+	path := "/verdicts/" + fmt.Sprintf("%d", judgeId)
+	req.Path(path)
+	req.SetHeader("Cookie", cookie)
+
+	return req.Send()
+}
+
+type recentSubmission struct {
+	Accuracy       string `json:"accuracy"`
+	CodeSize       int    `json:"codeSize"`
+	CpuTime        int    `json:"cpuTime"`
+	JudgeDate      int64  `json:"judgeDate"`
+	JudgeId        int64  `json:"judgeId"`
+	JudgeType      int    `json:"judgeType"`
+	Language       string `json:"language"`
+	Memory         int    `json:"memory"`
+	ProblemId      string `json:"problemId"`
+	ProblemTitle   string `json:"problemTitle"`
+	Status         int    `json:"status"`
+	SubmissionDate int64  `json:"submissionDate"`
+	Token          string `json:"token"`
+	UserId         string `json:"userId"`
+}
+
+func getRecentSubmissionRecords(cookie string) ([]recentSubmission, error) {
+	conf := config.GetConfig()
+
+	cli := gentleman.New()
+	cli.URL(conf.Endpoint)
+
+	req := cli.Request()
+	req.Method("GET")
+	req.Path("/submission_records/recent")
+	req.SetHeader("Cookie", cookie)
+
+	res, err := req.Send()
+	if err != nil {
+		return nil, err
+	}
+	if !res.Ok {
+		return nil, util.ErrResponseIsNotOK
+	}
+
+	var r []recentSubmission
+	if err := json.Unmarshal([]byte(res.String()), &r); err != nil {
+		return nil, err
+	}
+
+	return r, nil
 }
--- aoj/aoj_test.go
+++ aoj/aoj_test.go
@@ -1,6 +1,7 @@
 package aoj
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -8,17 +9,26 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSubmit(t *testing.T) {
+var cookie string
+
+func TestSubmitAndStatus(t *testing.T) {
 	problemId := "ITP1_1_A"
 	language := "C"
-	sourceCode := "#include \nint main(){\n printf(\"Hello World\\n\");\n return 0;\n}"
+	sourceCode := "#include <stdio.h> \nint main(){\nprintf(\"Hello World\\n\");\nreturn 0;\n}"
+	fmt.Println(sourceCode)
 
 	id := os.Getenv("AOJ_ID")
 	pass := os.Getenv("AOJ_RAWPASSWORD")
+
 	cookie, err := Session(id, pass)
 	require.NoError(t, err)
 	pp.Println(cookie)
+
 	token, err := Submit(cookie, problemId, language, sourceCode)
 	require.NoError(t, err)
 	pp.Println(token)
+
+	res, err := Status(cookie, token)
+	require.NoError(t, err)
+	pp.Println(res)
 }
